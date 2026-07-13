8d1e45acda0cfbb31f2c57318b1989015dd76ae2
hoon09290826-ship-it <hoon09290826@gmail.com>
2026-07-13 11:11:33 +0900

    Add files via upload

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bblotto-rc8-18';
+const CACHE_NAME = 'bblotto-rc9-1';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
